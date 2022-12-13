70d5c6296901945265eee09a935c9cdf40dc6495
Jun <tedxcdj@kakao.com>
2022-12-13 17:23:53 +0900

    DTO 만드는 이유와 service에 데이터 로직 처리 entity의 데이터 타입 정의 개념 이해

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,18 @@
-// eslint-disable-next-line prettier/prettier
-import { Controller,Post } from "@nestjs/common";
+import { Body, Controller, Post } from '@nestjs/common';
 import { UserService } from './user.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './entity/user.entity';
 
-@Controller('user')
+@Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
-  @Post()
-  signup(): string {
-    return this.userService.signup();
+  @Post('signup')
+  signup(@Body() createUserDto: CreateUserDto): User {
+    const { username, password } = createUserDto;
+    return this.userService.signup(createUserDto);
+  }
+  @Post('signin')
+  signin(@Body() createUserDto: CreateUserDto): boolean {
+    return this.userService.signin(createUserDto);
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,8 +1,35 @@
 import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './entity/user.entity';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UserService {
-  signup(): string {
-    return 'hi this is user';
+  private readonly users: User[] = [];
+  signup(createUserDto: CreateUserDto): User {
+    const { username, password } = createUserDto;
+    const saltRounds = 10;
+    const salt = bcrypt.genSaltSync(saltRounds);
+    const hash = bcrypt.hashSync(password, salt);
+    const user: User = {
+      username: username,
+      password: hash,
+      salt: salt,
+    };
+    this.users.push(user);
+    return user;
+  }
+  signin(createUserDto: CreateUserDto): boolean {
+    const { username, password } = createUserDto;
+    const saltRounds = 10;
+    const salt = bcrypt.genSaltSync(saltRounds);
+    let authCheck = false;
+    this.users.forEach((item) => {
+      const hash = bcrypt.hashSync(password, item.salt);
+      if (item.username == username && item.password == hash) {
+        authCheck = true;
+      }
+    });
+    return authCheck;
   }
 }
